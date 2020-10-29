eb85c149012964d4a283f83f25a17cb857b6f75c
Colby Thompson <colbyat@email.arizona.edu>
2020-10-29 11:55:38 -0700

    initial commit

--- ePatient/src/main/java/com/revature/dao/AuthDAOImpl.java
+++ ePatient/src/main/java/com/revature/dao/AuthDAOImpl.java
@@ -1,9 +1,5 @@
 package com.revature.dao;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
-
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
@@ -28,15 +24,8 @@ public class AuthDAOImpl implements AuthDAO {
 			s = HibernateSessionFactory.getSession();
 			tx = s.beginTransaction();
 			
-			/*CriteriaBuilder cb = s.getCriteriaBuilder();
-			CriteriaQuery<LoginEntry> cq = cb.createQuery(LoginEntry.class);
-			Root<LoginEntry> root = cq.from(LoginEntry.class);
-			cq.select(root).where(cb.and(cb.equal(root.get("username"), username),
-										cb.equal(root.get("password"), Encryptor.encrypt(password))));
-			
-			id = s.createQuery(cq).getSingleResult().getId();*/
-			
 			LoginEntry user = s.get(LoginEntry.class, username);
+			
 			if(user!=null && user.getPassword().equals(Encryptor.encrypt(password)))
 				id = user.getPerson().getId();
 			tx.commit();
