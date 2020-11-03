8c6bf3e5453c2167fd041b6409cfab331627f3bf
Rabinson Shrestha <rabinshrestha4100@gmail.com>
2020-11-03 12:13:19 -0600

    added @CrossOrigin to Medication and Prescription controller

--- ePatient/src/main/java/com/revature/controller/PrescriptionController.java
+++ ePatient/src/main/java/com/revature/controller/PrescriptionController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import com.revature.service.PrescriptionService;
 
 @RestController
 @RequestMapping("/prescription")
+@CrossOrigin
 public class PrescriptionController 
 {
 	private PrescriptionService ps;
